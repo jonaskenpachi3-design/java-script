a71dddf8e6e5bda7268fe13306c121d161dd4ea9
Jonas Sousa <jonaskenpachi3@gmail.com>
2026-05-28 19:49:37 -0500

    Add files via upload

---
+++ Repete.java
@@ -0,0 +1,13 @@
+public class Repete {
+    
+    public static void main(String[] args) throws Exception {
+        // While (enquano)
+        // While (<condição>){}
+        int contador = 0;
+        while (contador <3) {
+            System.err.println("Olá");
+            contador++;
+        }
+
+    }
+}
